dc15cba9192184b86d4add53e1d61dc3aeb9125f
Anton <mrwaitandsee@gmail.com>
2022-05-06 08:32:47 +0300

    new template

---
+++ tomcat/src/main/java/mrwaitandsee/userdomainapi/UserDomainApiApplication.java
@@ -0,0 +1,11 @@
+package mrwaitandsee.userdomainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDomainApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserDomainApiApplication.class, args);
+	}
+}
---
+++ tomcat/src/test/java/mrwaitandsee/userdomainapi/UserDomainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mrwaitandsee.userdomainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDomainApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
