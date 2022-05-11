2f6116f7fe7a31648eb83116d584956b44bdf617
Anton <mrwaitandsee@gmail.com>
2022-05-11 17:49:44 +0300

    netty template

---
+++ netty/src/main/java/mrwaitandsee/userdomainapi/UserdomainapiApplication.java
@@ -0,0 +1,11 @@
+package mrwaitandsee.userdomainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdomainapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserdomainapiApplication.class, args);
+	}
+}
